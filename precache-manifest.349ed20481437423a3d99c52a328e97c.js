768d2a083efe3eb295358eec72e32f5911fc4b26
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-25 01:06:20 +0530

    Updates

---
+++ precache-manifest.349ed20481437423a3d99c52a328e97c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70986e4fd209862be1126d91dbab6063",
+    "url": "/praveenorugantitech-github-search-reactjs/index.html"
+  },
+  {
+    "revision": "ab0ac595cb2e013fae78",
+    "url": "/praveenorugantitech-github-search-reactjs/static/css/2.100b230f.chunk.css"
+  },
+  {
+    "revision": "d9cf689314dc5df03b04",
+    "url": "/praveenorugantitech-github-search-reactjs/static/css/main.8e8f26b7.chunk.css"
+  },
+  {
+    "revision": "ab0ac595cb2e013fae78",
+    "url": "/praveenorugantitech-github-search-reactjs/static/js/2.910cb4fa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/praveenorugantitech-github-search-reactjs/static/js/2.910cb4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9cf689314dc5df03b04",
+    "url": "/praveenorugantitech-github-search-reactjs/static/js/main.017b0ab2.chunk.js"
+  },
+  {
+    "revision": "12f089ca2dd63137837e",
+    "url": "/praveenorugantitech-github-search-reactjs/static/js/runtime-main.0c538443.js"
+  }
+]);
